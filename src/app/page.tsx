fa3d3267158446b596936fc576abd0397bd5d1e6
AnaBizo <aninhadealmeida0502@gmail.com>
2025-01-29 12:01:09 -0300

    feat: criando grafico de barras com MUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,93 @@
-import Image from "next/image";
+"use client";
+import { useRef } from "react";
+import { BarChart } from "@mui/x-charts";
+import html2canvas from "html2canvas";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	const chartRef = useRef<HTMLDivElement>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	const exportAsImage = async () => {
+		if (!chartRef.current) return;
+
+		const canvas = await html2canvas(chartRef.current, {
+			backgroundColor: "rgba(0,0,0,0)",
+		});
+		const image = canvas.toDataURL("image/png");
+
+		// Criar link para download
+		const link = document.createElement("a");
+		link.href = image;
+		link.download = "grafico.png";
+		link.click();
+	};
+
+	return (
+		<div className="bg-white h-screen flex flex-col items-center justify-center">
+			<div
+				ref={chartRef}
+				className="p-4 shadow-lg rounded-lg"
+				style={{ backgroundColor: "transparent" }}
+			>
+				<BarChart
+					className="h-[410px]"
+					xAxis={[
+						{
+							id: "barCategories",
+							data: [
+								"Açucar",
+								"Alcool",
+								"Chá",
+								"Filtro de Café",
+								"Copo 180 ml",
+								"Papel Higiênico",
+								"Papel Sulfite",
+								"Papel Toalha",
+								"Água Sanitária",
+								"Desinfetante",
+								"Detergente Líquido",
+								"Sabonete Líquido",
+								"Folha EVA Amarela",
+								"Folha EVA Azul",
+								"Folha EVA Branca",
+								"Folha EVA Cinza",
+								"Folha EVA Laranja",
+								"Folha EVA Marrom",
+								"Folha EVA Preto",
+								"Folha EVA Rosa",
+								"Folha EVA Verde",
+								"Folha EVA Vermelha",
+								"Pasta Portifólio",
+							],
+							scaleType: "band",
+							tickLabelStyle: {
+								angle: 50,
+								textAnchor: "start",
+								fontSize: 12,
+							},
+						},
+					]}
+					series={[
+						{
+							data: [
+								100, 0, 100, 100, 50, 7, 43, 0, 100, 100, 100, 7, 50, 50, 50,
+								50, 50, 50, 50, 50, 50, 50, 100,
+							],
+							color: "#E86017",
+						},
+					]}
+					width={800}
+					height={300}
+					borderRadius={5}
+				/>
+			</div>
+
+			<button
+				type="button"
+				onClick={exportAsImage}
+				className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition"
+			>
+				Exportar como PNG
+			</button>
+		</div>
+	);
 }
